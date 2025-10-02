056873408b0223582f4eac551ceca3095aff43cd
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-02 19:08:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/button-click', (req, res) => {
-  console.log('Кнопка была нажата!');
-  res.json({ message: 'Сервер получил нажатие кнопки!' });
+app.post("/button-click", (req, res) => {
+  console.log("Кнопка была нажата!");
+  res.json({ message: "Сервер получил нажатие кнопки!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
