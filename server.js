66aaf8421fde89c93ac111e868a4cebdea0f0c75
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-07 12:20:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,13 +28,9 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    console.log("Запрос с origin:", origin);
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error("Not allowed by CORS"));
-  }
+  origin: true
 }));
+
 
 app.use(express.json());
 
