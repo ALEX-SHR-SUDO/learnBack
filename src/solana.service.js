0a570ab847f44de75ab01e3b23fdf2a9aa0b1ace
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-15 11:28:54 +0000

    qwqq

--- src/solana.service.js
+++ src/solana.service.js
@@ -17,9 +17,7 @@ import { addMetadataToToken as addMetadataStep } from "./metadata-addition.servi
 import { createUmi } from '@metaplex-foundation/umi'; // Базовый Umi
 import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
 import * as Umi from '@metaplex-foundation/umi'; 
-import { web3JsEddsa } from '@metaplex-foundation/umi-eddsa-web3js'; // Явный импорт EDDSA
-import { web3JsRpc } from '@metaplex-foundation/umi-rpc-web3js'; // Явный импорт RPC
-import { web3Js } from '@metaplex-foundation/umi-web3js'; // <-- НОВЫЙ ИМПОРТ
+import { web3Js } from '@metaplex-foundation/umi-web3js-adapters'; // <-- НОВЫЙ ИМПОРТ
 // Вам может потребоваться установить пакеты umi-eddsa-web3js и umi-rpc-web3js
 
 
