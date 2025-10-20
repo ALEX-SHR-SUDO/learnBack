0435b8be65e385db8aed647fd0b4c1af4edcf659
alex <alexshargo@icloud.com>
2025-10-20 18:13:42 +0300

    assssa

--- src/metadata-addition.service.js
+++ src/metadata-addition.service.js
@@ -77,13 +77,13 @@ function createMetaplexInstruction(params) {
     const metadataAddress = getMetadataAddress(mint);
 
     // --- 1. Подготовка структуры данных (DataV2) ---
-    const dataV2 = new DataV2({
+    const dataV2 = new mpl.DataV2({
         name: name,
         symbol: symbol,
         uri: uri,
         sellerFeeBasisPoints: 0,
         creators: [
-            new Creator({
+            new mpl.Creator({
                 address: owner,
                 verified: true,
                 share: 100
